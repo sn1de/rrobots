2779353be04d93d8ef65cdac2c4bcd35ce525d46
Brad Schneider <bschneider@containerstore.com>
2012-02-09 15:22:24 -0600

    Changing to require_relative to work under ruby 1.9.2

---
+++ lib/KeyboardDuck.rb
@@ -0,0 +1,45 @@
+require_relative 'rrobots/robot.rb'
+require 'rubygems'
+require 'gosu'
+
+class KeyboardDuck
+  include Robot
+
+  def tick(events)
+    unless (dist = events['robot_scanned']).empty?
+      say 'ENEMY SPOTTED ' + (dist.first.first / 2).to_i.to_s + ' PIXELS AWAY!'
+    end
+    
+    unless events['got_hit'].empty?
+      say 'OUCH!'
+    end
+
+    if events['button_pressed'].include?(Gosu::Button::KbNumpad1)
+        turn_radar 4
+    end
+    if events['button_pressed'].include?(Gosu::Button::KbNumpad3)
+        turn_radar -4
+    end
+    if events['button_pressed'].include?(Gosu::KbSpace)
+        fire 3
+    end
+    if events['button_pressed'].include?(Gosu::Button::KbNumpad8)
+        accelerate 3
+    end
+    if events['button_pressed'].include?(Gosu::KbUp)
+        accelerate -3
+    end
+    if events['button_pressed'].include?(Gosu::KbRight)
+        turn 4
+    end
+    if events['button_pressed'].include?(Gosu::KbLeft)
+        turn -4
+    end
+    if events['button_pressed'].include?(Gosu::Button::KbNumpad7)
+        turn_gun 4
+    end
+    if events['button_pressed'].include?(Gosu::Button::KbNumpad9)
+        turn_gun -4
+    end
+  end
+end
---
+++ lib/SnideBot.rb
@@ -0,0 +1,10 @@
+require_relative 'rrobots/robot.rb'
+
+class SnideBot 
+  include Robot
+  
+  def tick events
+    
+    
+  end
+end
