3519bc697771dfd68f49981cc299b9083ea33e5b
John Landes <jmlandes@containerstore.com>
2013-09-16 20:15:40 -0500

    jml robot ready for battle sir

--- lib/SharkBiscuit.rb
+++ lib/SharkBiscuit.rb
@@ -4,43 +4,44 @@ class SharkBiscuit
 	include Robot
 	
 	def tick events
-
+        # p "time is #{time}"
 		if events.empty?
-			turn_gun 12
+			adjust_gun -1
 		else 
-			p events
+			# p events
 		end
 
 		if !events['robot_scanned'].empty? 
 			# scanned something 
-			target_found
+			adjust_gun 1
 		end
 
 		if !events['got_hit'].empty?
 			evade
-			shoot
 		end
+
+        shoot
+        
+        if energy <= 10
+            say "I'm going towards the light, goodbye cruel world"
+        end
 
 
     end
+
+    def adjust_gun direction 
+        turn_gun 8 * direction 
+        # p "gun heading #{gun_heading}"
+
+    end
  
-    def target_found 
-    	#turn 5
-    	puts "found a target!!!"
-    	p "gun heading #{gun_heading}"
-    	shoot
-    	turn_gun -12
-    	accelerate(1)
-
-     end
-
     def evade 
-    	p "evading, speed is #{speed}, x is #{x}, y is #{y} and size #{size}"
-    	if x <= size
-    		turn -10
-    	else
-    		turn 10
-    	end
+    	# p "evading, heading #{heading}, speed is #{speed}, x is #{x}, y is #{y} and size #{size}"
+         if time % 2
+                turn 10
+            else
+                turn -10
+            end
 
  		if speed < 5 
  			accelerate 1
@@ -49,28 +50,11 @@ class SharkBiscuit
  		else
  			accelerate 1
  		end 
-    	turn_gun 5
     end
 
     def shoot
-    	p "gun heat #{gun_heat}"
-    	if @gun_heat < 0.25
-    		velocity = 2
-    	elsif @gun_heat > 0.75 
-    		velocity = 0.5
-    	else
-    		velocity = 0.1
-    	end
+        velocity = 0.1
     	fire(velocity)
-    	p "fire #{velocity}"
     end
 
-    def hit 
-    	#turn 1
-    	#turn_gun 30
-    	#turn_gun(30)
-    	stop
-    	accelerate -1
-    	p "ouch"
-    end
  end
